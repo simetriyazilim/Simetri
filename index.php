9084ba3f31144f65e2358739760b5b97f3a96f98
EmircanBayar <emircanbayarr21@gmail.com>
2016-05-23 14:12:54 +0300

    Site başlığı değişti

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang = "en">
 	<head>
 		<meta charset="utf-8" />
-		<title>Revelio Theme | Homepage</title>
+		<title>Simetri Yazılım</title>
 		<meta content="IE=edge" http-equiv="X-UA-Compatible" />
 		<meta content="width=device-width, initial-scale=1" name="viewport" />
 		<meta name="description" content="" />
